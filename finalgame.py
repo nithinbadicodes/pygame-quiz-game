863572c668c612816df197184cbe31b81979e896
nithinbadicodes <nbadi1350@gmail.com>
2026-03-05 13:13:40 +0530

    testing deploy branch

--- finalgame.py
+++ finalgame.py
@@ -15,8 +15,6 @@ pygame.font.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('v-shesh: Quiz on disability')
 clock = pygame.time.Clock()
-print('hello world')
-print('hello world')
 
 
 first_page = FrontPage(screen)
